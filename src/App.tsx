547208d25c78e28b471595aaab8a7e79d1ca4195
Cherevatyi Mykyta <nikita.cherevatyi@extern.a1.at>
2023-04-06 14:27:08 +0300

    Last-changes-1.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import CharacterWrapper from './components/characterWrapper/CharacterWrapper';
+
 import './App.css';
 
 function App() {
+  const firstRow = 'Hi there,'.split('');
+  const secondRow = 'I\'m Mykyta,'.split('');
+  const thirdRow = 'web developer'.split('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+      <main>
+        <h1 className="FontWeightNormal">
+          {firstRow.map((char) => {
+            return <CharacterWrapper char={char}/>;
+          })}
+          <br/>
+          {secondRow.map((char) => {
+            return <CharacterWrapper char={char}/>;
+          })}
+          <br/>
+          {thirdRow.map((char) => {
+            return <CharacterWrapper char={char}/>;
+          })}
+        </h1>
+        <p className="FrontendDeveloper FontWeightNormal">
+          Front End Developer
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button className="Button">Contact me!</button>
+      </main>
     </div>
   );
 }
---
+++ src/components/characterWrapper/CharacterWrapper.tsx
@@ -0,0 +1,28 @@
+import { FC, useState } from 'react';
+import logo from '../../logo-512x512.png';
+
+import './CharacterWrapper.css';
+
+type Props = {
+  char: string;
+}
+
+const CharacterWrapper: FC<Props> = ({ char }) => {
+  const [classArray, setClassArray] = useState(['Char']);
+
+  const animationHandler = () => {
+    setClassArray(['Char', 'animated', 'rubberBand']);
+    setTimeout(() => {
+      setClassArray(['Char']);
+    }, 1000);
+  }
+
+  if (char === ' ') {
+    return <>&nbsp;</>;
+  }
+  if (char === 'M') {
+    return <img alt="M" src={logo} width={73} height={75} className="Char-logo"/>;
+  }
+  return <span className={classArray.join(' ')} onMouseOver={animationHandler}>{char}</span>;
+}
+export default CharacterWrapper;
