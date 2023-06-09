51244f9311ce2c63eccf756ade9fa69898834c94
Mykytyi <nikita.cherevatyi@extern.a1.at>
2023-06-09 17:14:01 +0300

    Redid animation

--- src/components/welcomePage/WelcomePage.tsx
+++ src/components/welcomePage/WelcomePage.tsx
@@ -1,32 +1,38 @@
+import { useEffect, useState } from 'react';
 import CharacterWrapper from './characterWrapper/CharacterWrapper';
 
 import './WelcomePage.css';
 
 const WelcomePage = () => {
+  const [animationClass, setAnimationClass] = useState<string>('');
   const firstRow = 'Hi there,'.split('');
   const secondRow = 'I\'m Mykyta,'.split('');
   const thirdRow = 'web developer'.split('');
 
+  useEffect(() => {
+    setAnimationClass('AppearanceAnimation');
+  }, []);
+
   return (
-    <div className="WelcomePage">
+    <div className={`WelcomePage ${animationClass}`}>
       <div className="Wrapper">
         <h1>
-          {firstRow.map((char) => {
-            return <CharacterWrapper char={char}/>;
+          {firstRow.map((char, index) => {
+            return <CharacterWrapper char={char} num={index} row={1}/>;
           })}
           <br/>
-          {secondRow.map((char) => {
-            return <CharacterWrapper char={char}/>;
+          {secondRow.map((char, index) => {
+            return <CharacterWrapper char={char} num={index} row={2}/>;
           })}
           <br/>
-          {thirdRow.map((char) => {
-            return <CharacterWrapper char={char}/>;
+          {thirdRow.map((char, index) => {
+            return <CharacterWrapper char={char} num={index} row={3}/>;
           })}
         </h1>
         <p className="FrontendDeveloper">
           Front End developer / React expert
         </p>
-        {/* TODO work around it, maybe with a new "Contact me" page <button className="Button">Contact me!</button>*/}
+        <button className="Button"><a href="#contactMe">Contact me!</a></button>
       </div>
 
       <div className="AdditionalElements">
--- src/components/welcomePage/characterWrapper/CharacterWrapper.tsx
+++ src/components/welcomePage/characterWrapper/CharacterWrapper.tsx
@@ -1,14 +1,47 @@
-import { FC, useState } from 'react';
+import { FC, useState, useEffect } from 'react';
 import logo from '../../../logo-512x512.png';
 
 import './CharacterWrapper.css';
 
 type Props = {
   char: string;
+  num: number;
+  row: number;
 }
 
-const CharacterWrapper: FC<Props> = ({ char }) => {
-  const [classArray, setClassArray] = useState(['Char']);
+const CharacterWrapper: FC<Props> = ({ char , num, row}) => {
+  const [classArray, setClassArray] = useState(['']);
+  const [charLogoClass, setCharLogoClass] = useState(['Char-logo']);
+
+  useEffect(() => {
+    if (row === 1) {
+      setTimeout(() => {
+        setClassArray(['Char', 'animated', 'rubberBand']);
+      }, (num + 1) * 75);
+      setTimeout(() => {
+        setClassArray(['Char']);
+      }, (num + 1) * row + 2075);
+    } else if (row === 2) {
+      setTimeout(() => {
+        setClassArray(['Char', 'animated', 'rubberBand']);
+      }, ((num + 1) * 75 + 750));
+      setTimeout(() => {
+        setClassArray(['Char']);
+      }, (num + 1) * row + 2750);
+    } else if (row === 3) {
+      setTimeout(() => {
+        setClassArray(['Char', 'animated', 'rubberBand']);
+      }, ((num + 1) * 75 + 1500));
+      setTimeout(() => {
+        setClassArray(['Char']);
+      }, (num + 1) * row + 3500);
+    }
+    if (char === 'M') {
+      setTimeout(() => {
+        setCharLogoClass(['Char-logo', 'CharLogoAnimated']);
+      }, ((num + 1) * 75 + 800));
+    }
+  }, []);
 
   const animationHandler = () => {
     setClassArray(['Char', 'animated', 'rubberBand']);
@@ -21,10 +54,10 @@ const CharacterWrapper: FC<Props> = ({ char }) => {
     return <>&nbsp;</>;
   }
   if (char === 'M') {
-    return <img alt="Mykyta" src={logo} className="Char-logo"/>;
+    return <img alt="Mykyta" src={logo} className={charLogoClass.join(' ')}/>;
   }
   return <span
-    className={classArray.join(' ')}
+    className={`InitialCharacter ${classArray.join(' ')}`}
     onMouseOver={animationHandler}
   >{char}</span>;
 }
